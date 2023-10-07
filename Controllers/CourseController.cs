ba3a3ffc0dc77873339f9b18f49b3d4056b71811
Onur <iletonurdogan@gmail.com>
2023-10-07 21:16:39 +0300

    MVC Core ogrenmeye devam

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BtkAkademi.Models;
+
+namespace BtkAkademi.Controllers;
+
+public class CourseController : Controller
+{
+    public IActionResult index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult index(Candidate model)
+    {
+        return View();
+    }
+}
---
+++ Models/Candidate.cs
@@ -0,0 +1,20 @@
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        public string? Email {get; set;} = string.Empty;
+        public string? FirstName {get; set;} = string.Empty;
+        public string? LastName {get; set;} = string.Empty;
+        public string? FullName  => $"{FirstName} {LastName}";
+        public int? Age {get; set;}
+        public string? SelectedCourse {get; set;} = string.Empty;
+        public DateTime? ApplyAt {get; set;}
+
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+    }
+}
